f852d94093a2c324b372b03f9dbb70b0924e3e41
Yash <ymalegao@ucsc.edu>
2024-10-05 12:46:53 -0700

    step 2 done

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,29 @@ const gameName = "My super duper amazing game";
 document.title = gameName;
 
 const header = document.createElement("h1");
+const TotalCount = document.createElement("div")
+const buttonToClick = document.createElement("button");
+
+
 header.innerHTML = gameName;
+let counter: number = 0
+TotalCount.textContent= `${counter} Books`
 
-const buttonToClick = document.createElement("button")
-buttonToClick.id = "myButton"
-buttonToClick.addEventListener('click', (event: MouseEvent) => {
-    console.log(event)
-    console.log("you clicked me")
-})
-buttonToClick.innerHTML = "📖"
 
-app.append(header, buttonToClick);
+function updateDisplay(){
+    if (TotalCount){
+        TotalCount.textContent= `${counter} Books`
+    }
+}
+
+buttonToClick.id = "myButton";
+buttonToClick.addEventListener("click", () => {
+  console.log(counter);
+  counter++;
+  updateDisplay();
+});
+buttonToClick.innerHTML = "📖";
+
+
+
+app.append(header, buttonToClick, TotalCount);
