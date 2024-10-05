99c6fa84766301ef2821534cd175c2a6e210c911
Yash <ymalegao@ucsc.edu>
2024-10-05 12:58:59 -0700

    step 3 autoclicker compelte

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,29 @@ const gameName = "My super duper amazing game";
 document.title = gameName;
 
 const header = document.createElement("h1");
-const TotalCount = document.createElement("div")
+const TotalCount = document.createElement("div");
 const buttonToClick = document.createElement("button");
 
-
 header.innerHTML = gameName;
-let counter: number = 0
-TotalCount.textContent= `${counter} Books`
+let counter: number = 0;
+TotalCount.textContent = `${counter} Books`;
 
-
-function updateDisplay(){
-    if (TotalCount){
-        TotalCount.textContent= `${counter} Books`
-    }
+function updateDisplay() {
+  if (TotalCount) {
+    TotalCount.textContent = `${counter} Books`;
+  }
 }
+
+function autoClick(){
+    counter++;
+    updateDisplay();
+
+
+}
+
+let intervalID = setInterval(autoClick, 1000)
+console.log(intervalID)
+
 
 buttonToClick.id = "myButton";
 buttonToClick.addEventListener("click", () => {
@@ -28,7 +37,5 @@ buttonToClick.addEventListener("click", () => {
   updateDisplay();
 });
 buttonToClick.innerHTML = "📖";
-
-
 
 app.append(header, buttonToClick, TotalCount);
