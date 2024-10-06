69251c72de0c459d272a540baeef849f62501a79
Yash <ymalegao@ucsc.edu>
2024-10-06 14:50:05 -0700

    step 5 done

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,19 @@ document.title = gameName;
 const header = document.createElement("h1");
 const TotalCount = document.createElement("div");
 const buttonToClick = document.createElement("button");
+const upgradeButton = document.createElement("button")
+upgradeButton.id = "FirstUpdate"
+upgradeButton.disabled = true;
+
 
 header.innerHTML = gameName;
 let counter: number = 0;
 TotalCount.textContent = `${counter} Books`;
+
+
+
+
+
 
 function updateDisplay() {
   if (TotalCount) {
@@ -19,29 +28,31 @@ function updateDisplay() {
   }
 }
 
+let lastTime: number = 0;
+let growthRate: number = 0;
 
-let lastTime: number = 0
-
-function step(time:number){
-    if (!lastTime){
-        lastTime = time
-    }
-
-    const deltaTime = (time-lastTime) / 1000
-    const increment = deltaTime
-    counter += increment
-    updateDisplay();
+function step(time: number) {
+  if (!lastTime) {
     lastTime = time;
+  }
 
-    requestAnimationFrame(step)
+  const deltaTime = (time - lastTime) / 1000;
+  const increment = deltaTime * growthRate;
+  counter += increment;
+  updateDisplay();
+  lastTime = time;
 
+  if (counter >= 10 && upgradeButton.disabled){
+    upgradeButton.disabled = false;
+  }
+  if (counter < 10 && !upgradeButton.disabled){
+    upgradeButton.disabled = true;
 
+  }
+
+  requestAnimationFrame(step);
 }
-requestAnimationFrame(step)
-
-
-
-
+requestAnimationFrame(step);
 
 buttonToClick.id = "myButton";
 buttonToClick.addEventListener("click", () => {
@@ -49,6 +60,18 @@ buttonToClick.addEventListener("click", () => {
   counter++;
   updateDisplay();
 });
+
+upgradeButton.addEventListener("click", () => {
+    counter -=10
+    growthRate+=1
+
+  });
+upgradeButton.innerHTML = "UPGRADE BABY"
+
 buttonToClick.innerHTML = "📖";
 
-app.append(header, buttonToClick, TotalCount);
+
+
+
+
+app.append(header, buttonToClick, TotalCount, upgradeButton);
