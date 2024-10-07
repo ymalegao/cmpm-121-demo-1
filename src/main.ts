2274aca1cb1940bf8c8b659be0185f2a577388f0
Yash <ymalegao@ucsc.edu>
2024-10-06 18:59:15 -0700

    removed commented old stuff

--- src/main.ts
+++ src/main.ts
@@ -14,17 +14,6 @@ const crownDisplay = document.createElement("div");
 const gatherCrownsButton = document.createElement("button");
 const growthRateText = document.createElement("div");
 
-// const upgradeSerfs = document.createElement("button");
-// const serfsCountDisplay = document.createElement("div");
-// serfsCountDisplay.id = "serfID";
-
-// const upgradeKnights = document.createElement("button");
-// const knightsCountDisplay = document.createElement("div");
-// knightsCountDisplay.id = "knightsID";
-
-// const upgradeBards = document.createElement("button");
-// const bardsCountDisplay = document.createElement("div");
-// bardsCountDisplay.id = "bardID";
 
 let crowns: number = 0;
 let lastTime: number = 0;
@@ -32,21 +21,10 @@ let growthRate: number = 0;
 
 crownDisplay.textContent = `${crowns} Crowns`;
 growthRateText.textContent = `Current Influence Rate: ${growthRate} crowns/sec`;
-// upgradeSerfs.id = "SerfWorkforce";
-// upgradeKnights.id = "KnightsOfTheSquareTable";
-// upgradeBards.id = "SingingBards";
-// upgradeSerfs.disabled = true;
-// upgradeKnights.disabled = true;
-// upgradeBards.disabled = true;
 
 header.innerHTML = gameName;
 gatherCrownsButton.innerHTML = "🪙🪙🪙";
 document.title = gameName;
-
-// upgradeSerfs.innerHTML = " 'Hire' Serfs (10 Crowns)";
-
-// upgradeKnights.innerText = "Recruit Knights (100 Crowns)";
-// upgradeBards.innerText = "Singing Bards (1000 Crowns)";
 
 
 const availableItems: Item[] = [
@@ -77,7 +55,7 @@ availableItems.forEach(item =>{
             crowns -= upgrades[upgradeButton.id];
             growthRate += item.rate;
             playerItems[upgradeButton.id] += 1;
-            upgrades[upgradeButton.id] *= 1.15; // Increase cost for next purchase
+            upgrades[upgradeButton.id] *= 1.15; 
             updateDisplay();
             updatePlayerDisplay(upgradeButton.id);
         }
@@ -115,7 +93,6 @@ function step(time: number) {
   lastTime = time;
 
   checkUpgrades();
-  //   displayPlayerUpgrades();
 
   requestAnimationFrame(step);
 }
@@ -129,13 +106,6 @@ function checkUpgrades() {
     });
 }
 
-// function displayPlayerUpgrades() {
-//   Object.keys(playerItems).forEach((key) => {
-//     if (playerItems[key]){
-//         playerItems[key].innerText = `You possess ${playerItems[key]} ${key.replace(/.*?(.).*?([A-Z])/g, "$1$2").toLowerCase()} orders`;
-//     }
-//   });
-// }
 
 gatherCrownsButton.id = "myButton";
 gatherCrownsButton.addEventListener("click", () => {
@@ -144,49 +114,13 @@ gatherCrownsButton.addEventListener("click", () => {
   updateDisplay();
 });
 
-// upgradeSerfs.addEventListener("click", () => {
-//   crowns -= upgrades[upgradeSerfs.id];
-//   growthRate += 0.1;
-//   playerItems[upgradeSerfs.id] += 1;
-//   upgrades[upgradeSerfs.id] *= 1.15;
-//   console.log(upgrades);
-//   console.log(upgradeDisplays);
-//   console.log(playerItems);
-//   upgradeDisplays["SerfWorkforce"].innerText =
-//     `You possess ${playerItems["SerfWorkforce"]} Serf Workforces`;
-// });
-
-// upgradeKnights.addEventListener("click", () => {
-//   crowns -= upgrades[upgradeKnights.id];
-//   growthRate += 2.0;
-//   playerItems[upgradeKnights.id] += 1;
-//   upgrades[upgradeKnights.id] *= 1.15;
-
-//   upgradeDisplays["KnightsOfTheSquareTable"].innerText =
-//     `You possess ${playerItems["KnightsOfTheSquareTable"]} Knights`;
-// // });
-
-// upgradeBards.addEventListener("click", () => {
-//   crowns -= upgrades[upgradeBards.id];
-//   growthRate += 50.0;
-//   playerItems[upgradeBards.id] += 1;
-//   upgrades[upgradeBards.id] *= 1.15;
-
-//   upgradeDisplays["SingingBards"].innerText =
-//     `You possess ${playerItems["SingingBards"]} Bards`;
-// });
 
 app.append(
   header,
   growthRateText,
   gatherCrownsButton,
   crownDisplay,
-//   upgradeSerfs,
-//   upgradeKnights,
-//   upgradeBards,
-//   serfsCountDisplay,
-//   knightsCountDisplay,
-//   bardsCountDisplay,
+
 );
 requestAnimationFrame(step);
 
